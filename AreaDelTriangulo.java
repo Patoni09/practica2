2d9f306a3518ea54dd9caff544648d34db889138
Patoni09 <143773059+Patoni09@users.noreply.github.com>
2023-09-08 08:47:54 -0600

    Add files via upload

---
+++ AreaDelTriangulo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package area.del.triangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaDelTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+       //numerosa enteros
+       //numeros decimales
+        int base = 3;
+        int altura = 4;
+        int area = base * altura / 2;
+        
+        System.out.println("el area del triangulo es:" + area);
+    }
+    
+}
