21524bc6b726b3c9ccbeb6b409e2bc1fc52ae5a1
Patoni09 <143773059+Patoni09@users.noreply.github.com>
2023-09-08 20:49:18 -0600

    Add files via upload

---
+++ Lista.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista;
+
+/**
+ *
+ * @author ce182
+ */
+public class Lista {
+
+    public static void main(String[] args) {
+        String tarea1 = " lavar platos ";
+        String tarea2 = " estudiar ";
+        String tarea3 = " bañar a mi perro ";
+        String tarea4 = " despertar temprano ";
+         String tarea5 = " bañarme ";
+         
+         String cadenaTexto = "Cadena de Texto !!! %% $$$";
+         System.out.println("Mi tarea es" + tarea1 + tarea2 + tarea3 + tarea4 + tarea5 );
+    }
+}
